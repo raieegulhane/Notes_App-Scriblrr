5ccf63dd392adadcdba634b861c95eea4e56f13d
raieegulhane <gulhaneraiee17@gmail.com>
2022-05-24 11:52:20 +0530

    feat: navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import "./styles/styles.css"
 import SiteRoutes from "./routes/site-routes";
+import { TopNav } from "./components";
 
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
       <SiteRoutes />
     </div>
   );
---
+++ src/components/navigation/top-nav/index.jsx
@@ -0,0 +1,36 @@
+import "./top-nav.css";
+import { Link } from "react-router-dom";
+
+const TopNav = () => {
+    return (
+        <div className="wrapper-topnav">
+            <nav className="container-topnav flex-row flex_justify-sb flex_align-middle">
+                <div className="logoContainer">
+                    <Link className="logo link-noDecoration" to='/'>
+                        <h1>Scriblrr.</h1>
+                    </Link>
+                </div>
+                <div className="container-social flex-row">
+                    <a 
+                        className="btn-icon link-noDecoration" 
+                        href="https://twitter.com/RaieeGulhane" 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-twitter"></i>
+                    </a>
+                    <a 
+                        className="btn-icon link-noDecoration" 
+                        href="https://github.com/raieegulhane" 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-github-alt"></i>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export { TopNav };
