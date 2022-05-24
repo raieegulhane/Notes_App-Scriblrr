54df02a08696a3d46beccbf2330ebb4243ebd03f
raieegulhane <gulhaneraiee17@gmail.com>
2022-05-24 11:33:21 +0530

    added routes

--- src/routes/site-routes.jsx
+++ src/routes/site-routes.jsx
@@ -3,16 +3,18 @@ import Mockman from "mockman-js";
 import { Home, Archive, Labels, Trash, Login, Signup } from "../pages";
 
 const SiteRoutes = () => {
-    <Routes>
-        <Route path="/mockman" element={<Mockman />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/labels" element={<Labels />} />
-        <Route path="/trash" element={<Trash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-    </Routes>
+    return (
+        <Routes>
+            <Route path="/mockman" element={<Mockman />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/labels" element={<Labels />} />
+            <Route path="/trash" element={<Trash />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
+    );
 }
 
 export default SiteRoutes;
