235c28bb0e4389f833dbb00eb2112efb0f3806ea
raieegulhane <gulhaneraiee17@gmail.com>
2022-06-08 03:50:43 +0530

    feat: archive notes

--- src/pages/archive/index.jsx
+++ src/pages/archive/index.jsx
@@ -1,3 +1,20 @@
-const Archive = () => <div>Archive</div>
+import { SecondaryNav } from "../../components";
+import { useNote } from "../../contexts/note-context";
+import { NotesList } from "../../components/notes/notes-list";
+
+const Archive = () => {
+    const { noteState } = useNote();
+    const { archivedNotes } = noteState;
+
+    return(
+        <div className="flex-col">
+            <SecondaryNav />
+
+            <NotesList 
+                inputNotesArray={archivedNotes}
+            />
+        </div>
+    );
+}
 
 export { Archive };
