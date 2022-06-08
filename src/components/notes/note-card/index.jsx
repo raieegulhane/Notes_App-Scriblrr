14d26c84cf17995731f0a9584d587ac8aa2fe0ff
raieegulhane <gulhaneraiee17@gmail.com>
2022-06-08 15:19:26 +0530

    feat: delete notes

--- src/components/notes/note-card/index.jsx
+++ src/components/notes/note-card/index.jsx
@@ -5,12 +5,15 @@ import { useComponent } from "../../../contexts/component-context";
 import { useAuth } from "../../../contexts/auth-context";
 import { useNote } from "../../../contexts/note-context";
 import { postArchiveService } from "../../../services/archive-services/post-archive-service";
+import { deleteNoteService } from "../../../services/notes-services/delete-note-service";
 
 const NoteCard = ({ currentNote }) => {
-    const { _id, noteTitle, noteBody, noteColor } = currentNote;
+    const { _id, noteTitle, noteBody, noteColor, isArchived } = currentNote;
     const { authToken } = useAuth(); 
     const { componentDispatch } = useComponent();
     const { noteDispatch } = useNote();
+
+    const [noteValues, setNoteValues] = useState(currentNote);
 
     const [editorVisibility, setEditorVisibility] = useState(false);
     const showCardButtons = () => {
@@ -23,18 +26,31 @@ const NoteCard = ({ currentNote }) => {
     const editNoteHandler = () => {
         componentDispatch({ type: "SHOW_TEXT_EDITOR" });
         noteDispatch({ type: "EDIT_NOTE", payload: { editNoteStatus: true, editNoteId: _id}})
+        console.log(isArchived);
+    }
+
+    const deleteNoteHandler = async () => {
+        setNoteValues((currentNoteValues) => ({ ...currentNoteValues, isDeleted: true}));
+        console.log()
+        try {
+            const { data: { notes, trash} } = await deleteNoteService(noteValues, authToken);
+            noteDispatch({ type: "DELETE_NOTE", payload: { notes, trash } });
+        } catch (error) {
+            console.log("DELETE_NOTE_ERROR: ", error);
+        }
     }
 
     const archiveNoteHandler = async () => {
+        setNoteValues((currentNoteValues) => ({ ...currentNoteValues, isArchived: true}));
+
         try {
-            const { data: { archives, notes } } = await postArchiveService(currentNote, authToken);
-            noteDispatch({ type: "SET_ARCHIVE_NOTE", payload: { notes: notes, archives: archives }});            
+            const { data: { archives, notes } } = await postArchiveService(noteValues, authToken);
+            noteDispatch({ type: "SET_ARCHIVE_NOTE", payload: { notes, archives }});            
         } catch (error) {
             console.log("POST_ARCHIVE_ERROR: ", error);
         }
-
-        
     }
+
 
     return(
         <div 
@@ -62,7 +78,10 @@ const NoteCard = ({ currentNote }) => {
                 >
                     <i className="fa-solid fa-box-archive"></i>
                 </button>
-                <button className={`btn btn-icon editor-btn card-btn ${showCardButtons()}`}>
+                <button 
+                    className={`btn btn-icon editor-btn card-btn ${showCardButtons()}`}
+                    onClick={deleteNoteHandler}
+                >
                     <i className="fa-solid fa-trash-can"></i>
                 </button>
                 <button 
